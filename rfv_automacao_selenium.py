6ca56ace15b6c897a7a89725ffa43a37413fbc14
Darlan Monteiro <darlanmonteirott@gmail.com>
2025-06-26 22:03:41 -0300

    Update rfv_automacao_selenium.py

--- rfv_automacao_selenium.py
+++ rfv_automacao_selenium.py
@@ -21,60 +21,39 @@ def config_navegador():
     driver.get(site_rfv)
     return driver
 
-
-def automacao_rfv():
+def automacao_rfv(): # função principal para automatizar o processo de exportação de dados do RFV
     driver = config_navegador()
-    
-    # Carregar a base de clientes
     base_clientes = pl.read_excel(r'C:\Users\700543\Sotreq\Darlan Monteiro - Desenvolvimento\status_project\STATUS-DE-COMUNICACAO\clientes.xlsx')
     coluna_clientes = 'Clientes'
     clientes = base_clientes[coluna_clientes].to_list()
     
-    for cliente in clientes:
+    for cliente in clientes: # loop para iterar sobre cada cliente na lista de clientes
         try:
-            dropdown_abrir = WebDriverWait(driver, 120).until(
-                EC.visibility_of_element_located((By.XPATH, '//*[@id="involve-select-0"]/div[1]/input'))
-            )  
+            dropdown_abrir = WebDriverWait(driver, 120).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="involve-select-0"]/div[1]/input'))) # Localiza o dropdown de clientes
             dropdown_abrir.click()
             dropdown_abrir.clear()
             time.sleep(1)
-
             print(f"Procurando cliente: {cliente}")
             dropdown_abrir.send_keys(cliente)
-            time.sleep(1)  
+            time.sleep(0.3)  
             dropdown_abrir.send_keys(Keys.ENTER)
             print(f"Cliente {cliente} processado com sucesso.")
-            
         except TimeoutException:
-            print(f"Não foi possível encontrar o dropdown para o cliente {cliente}.")
-        
+            print(f"Não foi possível encontrar o dropdown para o cliente {cliente}.")       
         except Exception as e:
             print(f"Ocorreu um erro ao processar o cliente {cliente}: {e}")
-
         
-        try:
-            click_nome_cliente = WebDriverWait(driver, 120).until(
-                EC.element_to_be_clickable((By.XPATH, '//*[contains(@class, "involve-tree-node")]/h4'))
-            )
+        try: # Clica no nome do cliente na árvore de clientes
+            click_nome_cliente = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.XPATH, '//*[contains(@class, "involve-tree-node")]/h4')))
             time.sleep(1)
             click_nome_cliente.click()
             print(f"Nome do cliente {cliente} encontrado e clicado com sucesso.")
         except TimeoutException:
             print(f"Não foi possível encontrar o nome do cliente para {cliente}.")
             continue
-        
-               
-        
-        
          
-        try:
-            area_vitals_h4 = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((
-        By.XPATH,
-        '//div[contains(@class, "cdk-virtual-scroll-content-wrapper")]//h4[normalize-space()="Area Vitals" or normalize-space()="Fleet Status"]'
-    ))
-)
-
+        try: # Clica na área de Vitals ou Fleet Status
+            area_vitals_h4 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[contains(@class, "cdk-virtual-scroll-content-wrapper")]//h4[normalize-space()="Area Vitals" or normalize-space()="Fleet Status"]')))
             if "Area Vitals" in area_vitals_h4.text or "Fleet Status" in area_vitals_h4.text:
                 time.sleep(1)
                 area_vitals_h4.click()
@@ -82,15 +61,9 @@ def automacao_rfv():
         except TimeoutException:
             print("Não foi possível encontrar a área Vitals.")
             continue
-            
-            
-          
-        try: 
-            system_status = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((
-                By.CLASS_NAME,
-                'menu-button')))       
-    
+                       
+        try: # Clica no botão de status do sistema
+            system_status = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME,'menu-button')))       
             print('Clicando no status do sistema...')
             time.sleep(1)
             system_status.click()
@@ -99,22 +72,17 @@ def automacao_rfv():
             print("Não foi possível encontrar o status do sistema.")
             continue
         
-        try:
-            export = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//system-status-tile-v2//involve-datasource-export//button'))
-    )   
+        try: # Clica no botão de exportação de dados
+            export = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//system-status-tile-v2//involve-datasource-export//button')))   
             time.sleep(1)
             export.click()
-            print("Exportando dados...")
-            
+            print("Exportando dados...")    
         except TimeoutException:
             print("Não foi possível encontrar o botão de exportação.")
             continue
         
-        try:
-            xlsx = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CLASS_NAME, 'overflow-auto'))
-    )
+        try: # Seleciona o formato XLSX para exportação
+            xlsx = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'overflow-auto')))
             if "XLSX" in xlsx.text:
                 time.sleep(1)
                 xlsx.click()
@@ -122,10 +90,7 @@ def automacao_rfv():
         except TimeoutException:
             print("Não foi possível encontrar o formato XLSX.")
             continue
-        
         time.sleep(5)
-    
-
-         
+        
 automacao_rfv()
     
