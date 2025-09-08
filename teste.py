54a8011b87696c9a8da7a8553124173d392ed0b4
Darlan Monteiro <darlanmonteirott@gmail.com>
2025-09-08 11:30:18 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,9 +1,18 @@
-from pathlib import Path
+import polars as pl
+import datetime as dt
 
-# Pega a pasta do usuário
-user_docs = Path.home() / "Sotreq" / "Sol. Tec - Documentos" / "01 - Controle de Ativos"
+df = pl.DataFrame(
+    {
+        "name": ["Alice Archer", "Ben Brown", "Chloe Cooper", "Daniel Donovan"],
+        "birthdate": [
+            dt.date(1997, 1, 10),
+            dt.date(1985, 2, 15),
+            dt.date(1983, 3, 22),
+            dt.date(1981, 4, 30),
+        ],
+        "weight": [57.9, 72.5, 53.6, 83.1],  # (kg)
+        "height": [1.56, 1.77, 1.65, 1.75],  # (m)
+    }
+)
 
-# Caminho final
-caminho_bdativos = user_docs / "2 - Ativos Cat Connect.xlsm"
-
-print(caminho_bdativos)
+print(df)
